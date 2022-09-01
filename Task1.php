d18201046dc5d9aec7f1eadf9b7167fd3ad675d3
danchikov_a <danchikovartem@gmail.com>
2022-09-01 18:04:11 +0300

    Fixed double quotes

--- Task1.php
+++ Task1.php
@@ -4,10 +4,10 @@ namespace src;
 
 class Task1
 {
-    public const MORE_THAN_30_TEXT = "More than 30";
-    public const MORE_THAN_20_TEXT = "More than 20";
-    public const MORE_THAN_10_TEXT = "More than 10";
-    public const EQUAL_OR_LESS_THAT_10_TEXT = "Equal or less than 10";
+    public const MORE_THAN_30_TEXT = 'More than 30';
+    public const MORE_THAN_20_TEXT = 'More than 20';
+    public const MORE_THAN_10_TEXT = 'More than 10';
+    public const EQUAL_OR_LESS_THAT_10_TEXT = 'Equal or less than 10';
 
     public function main(int $number): string
     {
