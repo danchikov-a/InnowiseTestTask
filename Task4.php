5f7d6d703815e8d49df3639bce688867338e299a
danchikov_a <danchikovartem@gmail.com>
2022-09-01 10:57:18 +0300

    Created function that converts such strings with '_', '-', ' ' to single studly caps case words.

---
+++ Task4.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src;
+
+class Task4
+{
+    public function transformString(string $inputString): string
+    {
+        $separators = ['-', '_', ' '];
+
+        for ($i = 0; $i < strlen($inputString); $i++) {
+            if (in_array($inputString[$i], $separators)) {
+                $inputString = substr_replace($inputString, '', $i, 1);
+                $inputString[$i] = strtoupper($inputString[$i]);
+            }
+        }
+
+        return $inputString;
+    }
+}
