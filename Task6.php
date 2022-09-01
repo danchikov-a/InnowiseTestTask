20ea9f0b99e7ad7b10af6329b333363fb1ff2b21
danchikov_a <danchikovartem@gmail.com>
2022-09-01 18:08:20 +0300

    Fixed array syntax

--- Task6.php
+++ Task6.php
@@ -6,19 +6,19 @@ use InvalidArgumentException;
 
 class Task6
 {
-    const MONTHS = 12;
+    public const MONTHS = 12;
 
     public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
         if ($year < $lastYear || ($year == $lastYear && $month < $lastMonth)) {
-            $days = array(1 => "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+            $days = [1 => 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
             $amount = 0;
 
             for ($currentYear = $year; $currentYear <= $lastYear; $currentYear++) {
                 for ($currentMonth = $month;
-                     $currentMonth <= self::MONTHS && !($currentYear == $lastYear && $currentMonth == $lastMonth);
-                     $currentMonth++) {
-                    $firstDayOfMonth = date("w", strtotime("01.$currentMonth.$currentYear"));
+                    $currentMonth <= self::MONTHS && !($currentYear == $lastYear && $currentMonth == $lastMonth);
+                    $currentMonth++) {
+                    $firstDayOfMonth = date('w', strtotime("01.$currentMonth.$currentYear"));
 
                     if ($firstDayOfMonth == array_search($day, $days)) {
                         $amount++;
