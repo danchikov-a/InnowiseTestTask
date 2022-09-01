72818ce593c7108c98b26b34cdf08377dc1ae068
danchikov_a <danchikovartem@gmail.com>
2022-09-01 11:02:32 +0300

    Added function which decodes the JSON string.

---
+++ Task8.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src;
+
+use InvalidArgumentException;
+
+class Task8
+{
+    public function main(string $json): string
+    {
+        $jsonObject = json_decode($json);
+
+        if ($jsonObject != null) {
+            return sprintf("Title : %s\nAuthor : %s\nPublisher :%s",
+                $jsonObject->Title, $jsonObject->Author, $jsonObject->Detail->Publisher);
+        } else {
+            throw new InvalidArgumentException();
+        }
+    }
+}
