ac09c45cd139dcd7fba221696dbacaae380b2127
danchikov_a <danchikovartem@gmail.com>
2022-09-01 18:09:38 +0300

    Fixed sprintf output

--- Task8.php
+++ Task8.php
@@ -11,8 +11,12 @@ class Task8
         $jsonObject = json_decode($json);
 
         if ($jsonObject != null) {
-            return sprintf("Title : %s\nAuthor : %s\nPublisher :%s",
-                $jsonObject->Title, $jsonObject->Author, $jsonObject->Detail->Publisher);
+            return sprintf(
+                "Title : %s\nAuthor : %s\nPublisher :%s",
+                $jsonObject->Title,
+                $jsonObject->Author,
+                $jsonObject->Detail->Publisher
+            );
         } else {
             throw new InvalidArgumentException();
         }
