9446afa9fa52889daa77804b8f578e3b40265fd2
artem <danchikovartem@gmail.com>
2022-09-27 13:33:57 +0300

    Fixed configuration

--- app/models/Model.php
+++ app/models/Model.php
@@ -6,21 +6,25 @@ use PDO;
 
 class Model
 {
-    private const DB_HOST = 'myapp.cfg.DB_HOST';
-    private const DB_USER = 'myapp.cfg.DB_USER';
-    private const DB_PASS = 'myapp.cfg.DB_PASS';
-    private const CONFIG_PATH = '/app/config/php.ini';
+    private const DB_HOST = 'host';
+    private const DB_USER = 'user';
+    private const DB_PASS = 'password';
+    private const DB_CONFIG = 'db';
+    private const CONFIG_PATH = '/config/config.php';
 
     /**
      * @return PDO
      */
     protected static function getDB(): PDO
     {
+        $config = require $_SERVER['DOCUMENT_ROOT'] . self::CONFIG_PATH;
         static $db = null;
 
         if ($db === null) {
-            $credentials = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . self::CONFIG_PATH);
-            $db = new PDO($credentials[self::DB_HOST], $credentials[self::DB_USER], $credentials[self::DB_PASS]);
+            $db = new PDO($config[self::DB_CONFIG][self::DB_HOST],
+                $config[self::DB_CONFIG][self::DB_USER],
+                $config[self::DB_CONFIG][self::DB_PASS]
+            );
         }
 
         return $db;
---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'db' => [
+        'host' => 'mysql:host=localhost;port=3306;dbname=innowise',
+        'user' => 'root',
+        'password' => 'root0root',
+    ]
+];
