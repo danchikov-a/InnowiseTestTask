ea04d37c312264b0352e050e05c942767d60bd70
artem <danchikovartem@gmail.com>
2022-10-10 23:33:36 +0300

    Fixed uploadingFile const

--- app/controllers/FileController.php
+++ app/controllers/FileController.php
@@ -27,15 +27,16 @@ class FileController
 
     public function create(): void
     {
+        $uploadingFile = $_FILES["file"];
         //use at sign because all error messages are ignored so if such dir exists we won't see message on our page
         @mkdir(dirname(__DIR__, 2) . self::UPLOAD_DIRECTORY, 777);
         $directory = dirname(__DIR__, 2) . self::UPLOAD_DIRECTORY;
-        $file = $directory . basename($_FILES["file"]["name"]);
+        $file = $directory . basename($uploadingFile["name"]);
 
         $currentDirectorySize = $this->getDirectorySize($directory);
 
-        if ($currentDirectorySize + $_FILES["file"]["size"] < self::STORAGE_CAPACITY_BYTES) {
-            move_uploaded_file($_FILES["file"]["tmp_name"], $file);
+        if ($currentDirectorySize + $uploadingFile["size"] < self::STORAGE_CAPACITY_BYTES) {
+            move_uploaded_file($uploadingFile["tmp_name"], $file);
             unset($_SESSION['file_error']);
             $loggerMessage = "SUCCESS: file was uploaded.";
         } else {
@@ -44,7 +45,7 @@ class FileController
         }
 
         $formattedString = sprintf("%s %s %s %s",
-            $loggerMessage, $_FILES["file"]["name"], $_FILES["file"]["size"], date('d-m-y h:i:s'));
+            $loggerMessage, $uploadingFile["name"], $uploadingFile["size"], date('d-m-y h:i:s'));
         Logger::writeLog($formattedString);
 
         header("Location: /file");
